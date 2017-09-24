7fc618f4703ee1e5704b72764eb57831ac37ce3c
Chandresh Rajkumar Manonmani <cs1193@gmail.com>
2017-09-24 22:16:10 +0530

    LICENSE Added

--- source/Vultr/index.js
+++ source/Vultr/index.js
@@ -1,9 +1,20 @@
+/* @flow */
+
 import commander from 'commander';
+import progress from 'cli-progress';
 import {prompt} from 'inquirer';
 
 const VERSION = '1.0.1';
 const DESCRIPTION = 'Vultr API';
 
+const bar = new progress.Bar({}, progress.Presets.shades_classic)
+
+bar.start(100, 0);
+bar.update(100);
+bar.stop();
+
 commander
   .version(VERSION)
   .description(DESCRIPTION);
+
+commander.parse(process.argv);
