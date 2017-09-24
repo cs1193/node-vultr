5afb877780aed8f3f1481097af119cf0bd5cbdd3
Chandresh Rajkumar Manonmani <cs1193@gmail.com>
2017-09-24 23:47:43 +0530

    Directories Added

---
+++ source/Vultr/constant.js
@@ -0,0 +1,2 @@
+export const API_ENDPOINT = process.env.API_ENDPOINT || "https://api.vultr.com/";
+export const API_VERSION = process.env.API_VERSION || "v1";
--- source/Vultr/index.js
+++ source/Vultr/index.js
@@ -2,10 +2,15 @@
 
 import commander from 'commander';
 import progress from 'cli-progress';
+import log4js from 'log4js';
 import {prompt} from 'inquirer';
 
+import {API_ENDPOINT} from './constant';
+
+const logger = log4js.getLogger();
+
 const VERSION = '1.0.1';
-const DESCRIPTION = 'Vultr API';
+const DESCRIPTION = `Vultr API - ${API_ENDPOINT}`;
 
 const bar = new progress.Bar({}, progress.Presets.shades_classic)
 
